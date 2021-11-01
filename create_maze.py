e8d683080a243793f6210387f369e74a537869f2
Pham Phuoc <phhphc@gmail.com>
2021-11-01 22:36:11 +0700

    finish greedy best first search

---
+++ create_maze.py
@@ -0,0 +1,36 @@
+maze = [
+'''2
+3 6 -3
+5 14 -1
+xxxxxxxxxxxxxxxxxxxxxx
+x   x   xx xx        x
+x     x     xxxxxxxxxx
+x x   +xx  xxxx xxx xx
+  x   x x xx   xxxx  x
+x          xx +xx  x x
+xxxxxxx x      xx  x x
+xxxxxxxxx  x x  xx   x
+x          x x Sx x  x
+xxxxx x  x x x     x x
+xxxxxxxxxxxxxxxxxxxxxx''',
+'''2
+3 6 -3
+5 14 -1
+xxxxxxxxxxxxxxxxxxxxxx
+                     x
+x                    x
+x     +              x
+x                    x
+x             +      x
+x                    x
+x                    x
+x              S     x
+x                    x
+xxxxxxxxxxxxxxxxxxxxxx'''
+]
+
+n = int(input('Select maze: '))%len(maze)
+with open('maze_map.txt', 'w') as outfile:
+  outfile.write(maze[n])
+
+
--- search.py
+++ search.py
@@ -1,3 +1,6 @@
+from numpy import sqrt
+
+
 def BFS(matrix, start, end):
 
     previous = [[None]*len(matrix[0]) for i in range(len(matrix))]
@@ -18,7 +21,7 @@ def BFS(matrix, start, end):
     
 
     if previous[end[0]][end[1]] == None:
-        return []
+        return None
     else:
         (x, y) = end
         route = []
@@ -49,7 +52,7 @@ def DFS(matrix, start, end):
     
 
     if previous[end[0]][end[1]] == None:
-        return []
+        return None
     else:
         (x, y) = end
         route = []
@@ -60,6 +63,55 @@ def DFS(matrix, start, end):
 
         return route
     
+def GBFS(matrix, start, end): # greedy best first search
+
+    height, width = len(matrix), len(matrix[0])
+
+    def heuristic(x, y):
+        return sqrt((x - end[0])**2 + (y - end[1])**2)
+
+    previous = [[None]*width for i in range(height)]
+    h = [[None]*width for i in range(height)]
+    for i in range(height):
+        for j in range(width):
+            if matrix[i][j] != 'x':
+                h[i][j] = heuristic(i, j)
+    previous[start[0]][start[1]] = 0
+    print(previous)
+    print(h)
+
+    queue = [start]
+
+    def dequeue(queue):
+        n = 0
+        for i in range(len(queue)):
+            if h[ queue[i][0] ][ queue[i][1] ] < h[ queue[n][0] ][ queue[n][1] ]:
+                n = i
+        return queue.pop(n)
+
+    def move(x, y, prev): # if move to end return True
+        if matrix[x][y] != 'x' and previous[x][y] == None:
+            queue.append((x,y))
+            previous[x][y] = prev
+        return True if end == (x,y) else False
+
+    while len(queue) != 0:
+        (x, y) = dequeue(queue)
+        if move(x + 1,y,(x,y)) or move(x - 1,y,(x,y)) or move(x, y + 1,(x,y)) or move(x, y - 1, (x,y)):
+            break
+    
+
+    if previous[end[0]][end[1]] == None:
+        return None
+    else:
+        (x, y) = end
+        route = []
+        while previous[x][y] != 0:
+            route.insert(0, (x,y))
+            (x,y) = previous[x][y]
+        route.insert(0, start)
+
+        return route
              
 
      
